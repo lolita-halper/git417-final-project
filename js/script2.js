5897f8dcab7db38dd130e2cd56e50c8d21eeee37
lolita-halper <lhalper@asu.edu>
2024-06-24 20:16:30 -0700

    update

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function game() {
+    let dieDisplay1 = document.getElementById("random1");
+    let dieDisplay2 = document.getElementById("random2");
+    let gameMessage = document.getElementById("snakeEyesMsg");
+
+    let die1 = getRandomNumber(1, 6);
+    let die2 = getRandomNumber(1, 6);
+
+    dieDisplay1.innerHTML = die1;
+    dieDisplay2.innerHTML = die2;
+
+    if (die1 === 1 && die2 === 1) {
+        gameMessage.innerHTML = "Macaron! You Win!";
+    } else {
+        gameMessage.innerHTML = "You Lose. Try Again.";
+    }
+}
+
+document.getElementById("gamePlay").addEventListener("click", game);
+
